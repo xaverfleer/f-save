e8f78729bf5bfbbbcc9fd797d93a400756633067
Xaver Fleer <xaver.fleer@gmail.com>
2020-11-28 13:58:02 +0100

    Adapt description

--- src/appData.js
+++ src/appData.js
@@ -7,7 +7,7 @@ const appData = {
       price: 'CHF 2 / kg',
       contact: '077 44 66 505',
       description:
-        'Es macht ein falsches Gerücht die Runde, dieses Kartoffeln gäbe es nicht. Die Kartoffeln gibt es! Bitte Säcke/Harassen/Behälter selber mitbringen.',
+        'Es macht ein falsches Gerücht die Runde, dieses Kartoffeln gäbe es nicht. Die Kartoffeln gibt es! Die Kartoffeln gibt es in 1, 3 und 10 kg Säcken.',
       date: '28.11.2020',
     },
   ],
