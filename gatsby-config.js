1f182a00eb121edc8739eaff82b132fa3b0e99e8
Xaver Fleer <xaver.fleer@gmail.com>
2020-11-28 13:50:34 +0100

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'static/favicon.ico',
+        icon: 'static/favicon.png',
       },
     },
     'gatsby-plugin-react-helmet',
